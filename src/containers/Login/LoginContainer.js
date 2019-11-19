3d11737d2432dfa6345cff51958ba5377520b6f4
admuaq <admuaq@gmail.com>
2019-11-19 18:10:10 +0000

    added router and configured first route

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -6,13 +6,15 @@ import logo from '../../logo.svg'
 
 const LoginContainer = () => {
   return (
-    <div id='login' className=' form'>
-      {/* Put image logo here */}
-      <div className='container'>
-        <img className='logo' src={logo} alt='logo' />
-        <h1>TravelWrite</h1>
+    <div className='container'>
+      <div id='login' className=' form'>
+        {/* Put image logo here */}
+        <div className='container'>
+          <img className='logo' src={logo} alt='logo' />
+          <h1>TravelWrite</h1>
+        </div>
+        <Login />
       </div>
-      <Login />
     </div>
   )
 }
---
+++ src/containers/Post/PostContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+import Post from '../../components/Post/Post'
+// import './PostContainer.css'
+
+class PostContainer extends Component {
+  state = {
+    posts: [
+      { id: 1, name: 'First Post!', content: 'I cant believe that I am finally in Japan!' },
+      { id: 2, name: 'A new Journey', content: 'Hopping the waters to visit Saporo, Hokkaido!' },
+      { id: 3, name: 'Is this for real?', content: 'A world within worlds' },
+      { id: 4, name: 'Ronin: the story behind the name.', content: 'The samurai with a not so honourable story...' },
+      { id: 5, name: 'First JPop experience: Why you are missing out', content: 'One word: incredible!' },
+      { id: 6, name: 'Last week in Japan: Any suggestions?', content: 'What should I do within my last week in Japan?' },
+      { id: 7, name: 'Homebound', content: 'Last day in Japan' }
+    ],
+    selected: {}
+  }
+
+  switchPostHandler = () => {
+    const selectedPost = this.selectRandomPost()
+    console.log(selectedPost)
+    this.setState({selected: selectedPost})
+  }
+
+  selectRandomPost = () => {
+    return this.state.posts[(Math.floor(Math.random() * this.state.posts.length))]
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.switchPostHandler}>Click to switch Post</button> 
+        <div className='container'>
+          <Post 
+            postId={this.state.selected.id}
+            postName={this.state.selected.name}
+            postContent={this.state.selected.content}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostContainer
